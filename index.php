af8fb063e242c3e5ba36f3b2291b688d760b95d2
ikostov <ivelin.kostov88@abv.bg>
2015-12-25 20:42:58 +0200

    Test external folder

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     $cinnamon = new CinnamonPHP();
     $cinnamon->AddTemplatePath("./templates");
     $cinnamon->ForceRegenerateCache(TRUE);
+    $cinnamon->SetCacheDire("./cache2", TRUE);
     $test = "Hello  world!!!";
-    echo $cinnamon->LoadTemplate('template1.html', TRUE);
+    echo $cinnamon->LoadTemplate('template1.html', FALSE);
 ?>
